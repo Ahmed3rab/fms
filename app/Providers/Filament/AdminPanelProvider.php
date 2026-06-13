0bd866601de40edb11eb27b9faa19de7bde5c108
Ahmed Arab <me@ahmedarab.ly>
2026-06-13 20:11:30 +0200

    require 2fa

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -47,7 +47,7 @@ class AdminPanelProvider extends PanelProvider
                     ->multiFactorAuthentication([
                         AppAuthenticationProvider::make()
                             ->brandName(config('app.name')),
-                    ])
+                    ], true)
                     ->multiFactorRecovery(RecoveryCodeProvider::make()),
             ])
             ->middleware([
