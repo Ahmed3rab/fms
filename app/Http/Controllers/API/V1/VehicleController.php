8e9070edc5b47ef3be4b0935a9b9be188a879980
Ahmed Arab <me@ahmedarab.ly>
2026-06-21 21:58:46 +0200

    add methods to vehicle controller

--- app/Http/Controllers/API/V1/VehicleController.php
+++ app/Http/Controllers/API/V1/VehicleController.php
@@ -2,11 +2,46 @@
 
 namespace App\Http\Controllers\API\V1;
 
+use App\Filters\VehicleFilter;
+use App\Http\Resources\API\V1\VehicleResource;
+use App\Models\Vehicle;
 use App\Http\Controllers\Controller;
+use App\Services\Tracking\Contracts\TrackingGateway;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
 
 class VehicleController extends Controller
 {
-    public function index() {}
-    public function show() {}
+    public function index(Request $request, TrackingGateway $tracking): AnonymousResourceCollection
+    {
+        $query = (new VehicleFilter(
+            Vehicle::query()
+                ->visibleTo(auth()->user())
+                ->with([
+                    'company',
+                    'device.state',
+                ]),
+            $request->all(),
+        ))->apply();
+
+        $vehicles = $query->paginate();
+
+        $tracking->hydrateVehicles(
+            $vehicles->getCollection()
+        );
+
+        return VehicleResource::collection($vehicles);
+    }
+
+    public function show(Vehicle $vehicle, TrackingGateway $tracking): VehicleResource
+    {
+        $vehicle->load([
+            'company',
+            'device.state',
+        ]);
+
+        return VehicleResource::make(
+            $tracking->hydrateVehicle($vehicle)
+        );
+    }
 }
