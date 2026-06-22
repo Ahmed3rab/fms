fbaefc9988497387a6e7e93449185ea1ac37a15c
Ahmed Arab <me@ahmedarab.ly>
2026-06-22 09:49:56 +0200

    Add Geocoding contract

---
+++ app/Services/Geocoding/Contracts/Geocoder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Services\Geocoding\Contracts;
+
+interface Geocoder
+{
+    /**
+     * Reverse geocode coordinates.
+     *
+     * @return array<string,mixed>|null
+     */
+    public function reverse(
+        float $latitude,
+        float $longitude,
+        ?string $language = null,
+    ): ?array;
+}
