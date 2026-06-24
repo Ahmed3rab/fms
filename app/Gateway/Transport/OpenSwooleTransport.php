2a9685bd2df48edba3e72cedf03ec3ce4a67f052
Ahmed Arab <me@ahmedarab.ly>
2026-06-24 11:02:36 +0200

    prepare OpenSwooleTransport class

--- app/Gateway/Transport/OpenSwooleTransport.php
+++ app/Gateway/Transport/OpenSwooleTransport.php
@@ -6,6 +6,8 @@ use App\Gateway\Connections\Connection;
 use App\Gateway\Gateway;
 use App\Gateway\Transport\Contracts\GatewayTransport;
 use App\Gateway\Connections\ConnectionFactory;
+use OpenSwoole\Http\Request;
+use OpenSwoole\WebSocket\Frame;
 use OpenSwoole\WebSocket\Server;
 
 class OpenSwooleTransport implements GatewayTransport
@@ -28,8 +30,12 @@ class OpenSwooleTransport implements GatewayTransport
     public function start(Gateway $gateway): void
     {
         $this->createServer();
+
+        $this->registerEventHandlers($gateway);
+
         $this->server->start();
     }
+
 
     public function stop(): void {}
 
@@ -47,4 +53,37 @@ class OpenSwooleTransport implements GatewayTransport
             'heartbeat_check_interval' => 30,
         ]);
     }
+
+    protected function registerEventHandlers(Gateway $gateway): void
+    {
+        $this->server->on('Open', function (Server $server, Request $request) use ($gateway): void {
+            $this->handleOpen($gateway, $server, $request);
+        });
+
+        $this->server->on('Message', function (Server $server, Frame $frame) use ($gateway): void {
+            $this->handleMessage($gateway, $server, $frame);
+        });
+
+        $this->server->on('Close', function (Server $server, int $fd) use ($gateway): void {
+            $this->handleClose($gateway, $server, $fd);
+        });
+    }
+
+    protected function handleOpen(
+        Gateway $gateway,
+        Server $server,
+        Request $request,
+    ): void {}
+
+    protected function handleMessage(
+        Gateway $gateway,
+        Server $server,
+        Frame $frame,
+    ): void {}
+
+    protected function handleClose(
+        Gateway $gateway,
+        Server $server,
+        int $fd,
+    ): void {}
 }
