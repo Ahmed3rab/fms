2e02c5f227d65fb0920a8efd2de600b90bf0e19e
Ahmed Arab <me@ahmedarab.ly>
2026-06-27 21:17:55 +0200

    WIP

--- lang/vendor/profile-filament/pt_BR/pages/settings/delete-account/component.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-declare(strict_types=1);
-
-return [
-    'heading' => 'Excluir conta',
-
-    'description' => 'Uma vez que você exclua sua conta, todos os seus dados e recursos serão permanentemente deletados. Não seremos capazes de recuperar nenhum dos seus dados.',
-];
