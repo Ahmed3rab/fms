05f0f26c2d646a424053c956466428021b5f39b3
Ahmed Arab <me@ahmedarab.ly>
2026-06-29 23:49:06 +0200

    Fix timestamps timezone

--- app/Http/Resources/API/V1/TrackingTimestampResource.php
+++ app/Http/Resources/API/V1/TrackingTimestampResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\API\V1;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
 
 /**
  * @mixin \App\Data\TrackingTimestamps
@@ -18,9 +19,15 @@ class TrackingTimestampResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'gps' => $this->gps(),
-            'received' => $this->received(),
-            'last_synced' => $this->lastSynced(),
+            'gps' => $this->format($this->gps()),
+            'received' => $this->format($this->received()),
+            'last_synced' => $this->format($this->lastSynced()),
         ];
+    }
+
+    protected function format(?Carbon $date): ?string
+    {
+        return $date?->setTimezone(config('app.timezone'))
+            ->toIso8601String();
     }
 }
